cc9c0d46f4bfa64d90cbe5a56fd4d0a000dd2d57
ArunZemoso <arunkumar.marella@zemosolabs.com>
2022-05-09 01:11:11 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+import CharactersList from "./pages/CharactersList.js";
+import Character from "./pages/Character.js";
+import Search from "./pages/Search.js";
+
+import {Routes, Route} from "react-router";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route strict exact path="/" component={CharactersList} />
+        <Route strict exact path="/search" component={Search} />
+        <Route strict exact path="/:id" component={Character} />
+      </Routes>
     </div>
   );
 }
